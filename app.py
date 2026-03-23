8fc09f5650d48e06fb095f2a6345571784773f35
Xstoneekwa <xstonekwa@hotmail.com>
2026-03-23 21:41:26 +0200

    origin test-pr-trigger

--- app.py
+++ app.py
@@ -1,11 +1,1 @@
-from fastapi import FastAPI
-
-app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"message": "Hello World"}
-
-@app.get("/users")
-def get_users():
-    return {"users": ["Alice", "Bob"]}
+git add test-webhook.txt
