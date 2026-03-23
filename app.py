b8862cb612fd1339e3668a81f3c73d702679dde7
Xstoneekwa <xstonekwa@hotmail.com>
2026-03-23 12:10:38 +0200

    add new endpoints for testing PR automation

--- app.py
+++ app.py
@@ -9,3 +9,13 @@ def home():
 @app.get("/users")
 def get_users():
     return {"users": ["Alice", "Bob"]}
+@app.get("/users/v1")
+def get_users_v1():
+    return {"users": ["Alice", "Bob"], "version": "v1"}
+    @app.get("/messages")
+def messages():
+    return {"message": "ok"}
+
+@app.get("/messages/v1")
+def messages_v1():
+    return {"message": "ok"}
